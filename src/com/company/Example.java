c9656df1ea402c693dc8d68d39e284bc96ccaefc
Ian de Jesus <ian.dejesus.md@gmail.com>
2017-04-15 08:50:04 +0900

    Stack Trace and Call Stack

---
+++ src/com/company/Example.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Created by idejesus on 12/04/2017.
+ */
+public class Example {
+    public static void main(String[] args) {
+        int result = divide();
+        System.out.println(result);
+
+    }
+
+    private static int divide() {
+
+            int x = getInt();
+            int y = getInt();
+            System.out.println("x is " + x + ", y is " + y);
+            return x / y;
+
+
+    }
+
+    private static int getInt() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an integer ");
+        return s.nextInt();
+    }
+
+
+}
